7248a1b9768e4ba2c30013ffcf72ec6cef793e82
Dev-Suvo <subodipdey20@gmail.com>
2026-06-24 08:09:30 +0530

    code added

---
+++ leet/Anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # frq_s = Counter(s)
+        # frq_t = Counter(t)
+
+        # if frq_s == frq_t :
+        #     return True
+
+        # return False  
