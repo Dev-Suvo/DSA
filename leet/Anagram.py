9e95fcf36c5c6e96a86b89700e20e93b051439e2
Dev-Suvo <subodipdey20@gmail.com>
2026-06-24 08:36:40 +0530

    code added

--- leet/Anagram.py
+++ leet/Anagram.py
@@ -7,3 +7,15 @@ class Solution:
         #     return True
 
         # return False  
+
+        if len(s) != len(t):
+            return False
+
+        countS , countT = {} , {}
+
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i],0) + 1
+            countT[t[i]] = countT.get(t[i],0) + 1
+
+        return countS == countT
+        
