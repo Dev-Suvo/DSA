dfaa571003e218fd69cf45a7d78896112a97493b
Dev-Suvo <subodipdey20@gmail.com>
2026-06-29 11:43:57 +0530

    code added

--- leet/twosum.py
+++ leet/twosum.py
@@ -1,11 +1,11 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:        
-        prvhash={}
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
 
-        for idx, i in enumerate(nums):
-            diff = target - i
+        for idx , i in enumerate(nums):
+            need = target - i
 
-            if diff in prvhash:
-                return [prvhash[diff], idx]
+            if need in map:
+                return [idx, map[need]]
 
-            prvhash[i]=
+            map[i] = idx    
