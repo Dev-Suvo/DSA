e31a27bea285992a0aaaa4eeaece48f0b1f8dac3
Dev-Suvo <subodipdey20@gmail.com>
2026-06-30 12:30:26 +0530

    code added

--- leet/Anagram.py
+++ leet/Anagram.py
@@ -1,21 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # frq_s = Counter(s)
-        # frq_t = Counter(t)
-
-        # if frq_s == frq_t :
-        #     return True
-
-        # return False  
+        counts, countt = {}, {}
 
         if len(s) != len(t):
             return False
 
-        countS , countT = {} , {}
-
         for i in range(len(s)):
-            countS[s[i]] = countS.get(s[i],0) + 1
-            countT[t[i]] = countT.get(t[i],0) + 1
+            counts[s[i]] = counts.get(s[i],0) + 1
+            countt[t[i]] = countt.get(t[i],0) + 1
 
-        return countS == countT
-        
+        return counts == countt
