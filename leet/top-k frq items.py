bb17f2ab8595fcd8a671491f6423df870e4d70fc
Dev-Suvo <subodipdey20@gmail.com>
2026-07-02 20:28:52 +0530

    code added

--- leet/top-k frq items.py
+++ leet/top-k frq items.py
@@ -1,17 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
-        frq = [[] for _ in range(len(nums)+1)]  
+        frq = [[] for _ in range(len(nums)+1)]
 
         for n in nums:
-            count[n] = count.get(n,0) +1
+            count[n] = count.get(n,0)+1
 
         for n,c in count.items():
             frq[c].append(n)
-        
+
         res = []
         for i in range(len(frq)-1,0,-1):
             for n in frq[i]:
                 res.append(n)
-                if len(res) ==k:
+                if len(res) == k:
                     return res
