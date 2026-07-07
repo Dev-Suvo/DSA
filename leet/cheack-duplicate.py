0d5e946670b2bf6654f72704166dc61ad2409ec0
Dev-Suvo <subodipdey20@gmail.com>
2026-07-07 17:16:34 +0530

    code added

--- leet/cheack-duplicate.py
+++ leet/cheack-duplicate.py
@@ -1,5 +1,15 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        set_num = set(nums)
+        # set_num = set(nums)
 
-        return not(len(set_num) == len(nums))
+        # return not(len(set_num) == len(nums))
+
+        hash = {}
+
+        for n in nums:
+            hash[n] = hash.get(n,0) +1
+
+        for val in hash.values():
+            if val > 1 :
+                return True
+        return False
