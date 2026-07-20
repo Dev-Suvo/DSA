7cacdded755de3817f9b9217df13c2bd1a97ac84
Dev-Suvo <subodipdey20@gmail.com>
2026-07-20 19:52:30 +0530

    code added

--- leet/cheack-duplicate.py
+++ leet/cheack-duplicate.py
@@ -4,4 +4,13 @@ class Solution:
 
         # return not(len(set_num) == len(nums))
 
-        
+
+        map = {}
+
+        for n in nums:
+            map[n] = map.get(n,0)+1
+
+        for c in map.values():
+            if c > 1:
+                return True
+        return False
