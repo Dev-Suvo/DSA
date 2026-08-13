e94493c2f50c801f3488279b614cdd3fb87a98a2
Dev-Suvo <subodipdey20@gmail.com>
2026-08-13 17:29:26 +0530

    code added

--- leet/valid sudoku.py
+++ leet/valid sudoku.py
@@ -1,23 +1,24 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        col = defaultdict(list)
         row = defaultdict(list)
-        sq = defaultdict(list)
+        col = defaultdict(list)
+        box = defaultdict(list)
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
                     continue
 
-                if(
-                    board[r][c] in row[r] or
-                    board[r][c] in col[c] or
-                    board[r][c] in sq[(r//3, c//3)]):
+                if( board[r][c] in row or
+                    board[r][c] in col or
+                    board[r][c] in box[(r//3, c//3)]
+                    ):
 
                     return False
 
                 row[r].append(board[r][c])
                 col[c].append(board[r][c])
-                sq[(r//3,c//3)].append(board[r][c])
+                box[(r//3, c//3)].append(board[r][c])
+
 
         return True
