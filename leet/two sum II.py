cc0ee139f6ccc85d2323af73574e0886f2ca003d
Dev-Suvo <subodipdey20@gmail.com>
2026-08-20 21:08:54 +0530

    code added

--- leet/two sum II.py
+++ leet/two sum II.py
@@ -1,16 +1,16 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l= 0
+        l = 0
         r = len(numbers)-1
 
-        while l<r:
-            curSum = numbers[l] + numbers[r]
+        while l<r :
+            current = numbers[l] + numbers[r]
 
-            if curSum > target:
-                r-=1
+            if current > target:
+                r -=1
 
-            if  curSum < target:
-                l-=1
+            if current < target:
+                l +=1
 
-            if  curSum == target:
+            if current == target:
                 return [l+1,r+1]
