98d59e22c84a77e78b732d451320350507a0229e
Dev-Suvo <subodipdey20@gmail.com>
2026-09-11 21:12:32 +0530

    code added

---
+++ leet/JS/d5.js
@@ -0,0 +1,10 @@
+const JsUser = {
+    name: "Hitesh",
+    "full_name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
