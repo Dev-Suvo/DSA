0f5ffb3dded91a5932668f2e3f0b71817aa8da8a
Dev-Suvo <subodipdey20@gmail.com>
2026-09-11 21:18:24 +0530

    code added

--- leet/JS/d5.js
+++ leet/JS/d5.js
@@ -1,10 +1,20 @@
+const mySym = Symbol('key1')
+
+
 const JsUser = {
-    name: "Hitesh",
-    "full_name": "Hitesh Choudhary",
+    name: "Suvo",
+    "full_name": "Suvo Dey",
     [mySym]: "mykey1",
     age: 18,
-    location: "Jaipur",
+    location: "wb",
     email: "hitesh@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
+
+console.log(JsUser);
+
+console.log(JsUser.name);
+console.log(JsUser['email']);
+
+console.log(JsUser[mySym]);
