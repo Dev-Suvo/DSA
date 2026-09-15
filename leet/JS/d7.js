b5bdb8e4d7b9935f7e4c092af1cb9622dcb4dca1
Dev-Suvo <subodipdey20@gmail.com>
2026-09-15 21:12:31 +0530

    code added

--- leet/JS/d7.js
+++ leet/JS/d7.js
@@ -1,4 +1,4 @@
-
-for (let i = 0; i < Array.length; i++){
-    const ele = Array[i]
+let arr = [20,"suvo", 40, 50, [1,2,3],{name:"suvo",age:20}]
+for (let i = 0; i < arr.length; i++){
+    const ele = arr[i]
 }
