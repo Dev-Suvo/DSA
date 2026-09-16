81224c3f107a1438b6a67736cc5065b047c2135d
Dev-Suvo <subodipdey20@gmail.com>
2026-09-16 22:15:19 +0530

    code added

--- leet/JS/d8.js
+++ leet/JS/d8.js
@@ -1,10 +1,11 @@
 userobj = {
     name: "suvo",
     age: 23,
+    salary: 25000
 }
 
 function APIDATA(api) {
-    return `Name is ${api.name} and age is ${api.age}`
+    return `Name is ${api.name} and age is ${api.age} and salary is ${api.salary}`
 }
 
-console.log(APIDATA(user));
+console.log(APIDATA(userobj));
