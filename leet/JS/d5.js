4b2225736c17b72b5a24c77671c14cc77a7e360f
Dev-Suvo <subodipdey20@gmail.com>
2026-09-23 23:30:54 +0530

    code added

---
+++ leet/JS/d5.js
@@ -0,0 +1,32 @@
+const mySym = Symbol('key1')
+
+
+const JsUser = {
+    name: "Suvo",
+    "full_name": "Suvo Dey",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "wb",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser);
+
+// console.log(JsUser.name);
+// console.log(JsUser['email']);
+
+// console.log(JsUser[mySym]);
+
+
+JsUser.greeting = function () {
+    console.log(JsUser);
+}
+
+JsUser.greetingTwo = function () {
+    console.log(`hello ${this.name}`);
+}
+
+JsUser.greeting()
+console.log(JsUser.greetingTwo()); //log makes and extra `undefined`
